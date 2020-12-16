acbd1d39852eb828358c07c2b79cb3b9aab9673b
Mark Music <markmusic999@gmail.com>
2020-12-15 20:37:30 -0600

    fea(main.py): 🎃 Add getXCoefficient and getYCoefficient methods to class

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from random import randint
 
 
-def generateEquation(minNum, maxNum):
-    xCoefficient = randint(minNum, maxNum)
-    yCoefficient = randint(minNum, maxNum)
-    interval = yCoefficient - xCoefficient
-    constant = yCoefficient + interval
-    print(f"{xCoefficient}x + {yCoefficient}y = {constant}")
+class LinearEquationPattern:
+    def __init__(self, minNum, maxNum):
+        self.minNum = minNum,
+        self.maxNum = maxNum,
 
+    def generateEquation(self):
+        xCoefficient = randint(self.minNum[0], self.maxNum[0])
+        yCoefficient = randint(self.minNum[0], self.maxNum[0])
+        interval = yCoefficient - xCoefficient
+        constant = yCoefficient + interval
+        return f"{xCoefficient}x + {yCoefficient}y = {constant}"
 
-generateEquation(0, 100)
+    def getXCoefficient(self, eq):
+        i = 0
+
+        while i < len(eq):
+            if eq[i] == "x":
+                return eq[i - 1]
+
+            i += 1
+
+    def getYCoefficient(self, eq):
+        i = 0
+
+        while i < len(eq):
+            if eq[i] == "y":
+                return eq[i - 1]
+
+            i += 1
+
+    def solveSystem(self):
+        eq1 = self.generateEquation()
+        eq2 = self.generateEquation()
